bc01c447c4655f553bc2c11ea320b06682c341ce
chibuzor darlington <darlingtonchibuzor64@gmail.com>
2020-08-20 00:19:35 +0100

    darlington search and sorting solution submission

---
+++ Darlington/phase2/Search and Sorting/day 62 solution/qtn3.py
@@ -0,0 +1,16 @@
+# program to sort a list of elements using the insertion sort algorithm.
+def insertionSort(nlist):
+   for index in range(1,len(nlist)):
+
+     currentvalue = nlist[index]
+     position = index
+
+     while position>0 and nlist[position-1]>currentvalue:
+         nlist[position]=nlist[position-1]
+         position = position-1
+
+     nlist[position]=currentvalue
+
+nlist = [14,46,43,27,57,41,45,21,70]
+insertionSort(nlist)
+print(nlist)
